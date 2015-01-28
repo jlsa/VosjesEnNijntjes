eafded8cfc0faa60ad98425959b00a4396d1f8ab
atmino <angelanieboer@gmail.com>
2015-01-28 10:36:31 +0100

    Bears
    Transmuted foxes into bears.

--- week7/src/Bear.java
+++ week7/src/Bear.java
@@ -12,33 +12,33 @@ import java.util.Random;
  */
 public class Bear extends Animal implements Actor
 {
-    // Characteristics shared by all foxes (class variables).
+    // Characteristics shared by all bears (class variables).
     
-    // The age at which a fox can start to breed.
+    // The age at which a bear can start to breed.
     private static final int BREEDING_AGE = 20;
-    // The age to which a fox can live.
+    // The age to which a bear can live.
     private static final int MAX_AGE = 200;
-    // The likelihood of a fox breeding.
+    // The likelihood of a bear breeding.
     private static final double BREEDING_PROBABILITY = 0.08;
     // The maximum number of births.
     private static final int MAX_LITTER_SIZE = 2;
     // The food value of a single rabbit. In effect, this is the
-    // number of steps a fox can go before it has to eat again.
+    // number of steps a bear can go before it has to eat again.
     private static final int RABBIT_FOOD_VALUE = 12;
     // A shared random number generator to control breeding.
     private static final Random rand = Randomizer.getRandom();
     
     // Individual characteristics (instance fields).
-    // The fox's age.
+    // The bear's age.
     private int age;
-    // The fox's food level, which is increased by eating rabbits.
+    // The bear's food level, which is increased by eating rabbits.
     private int foodLevel;
 
     /**
-     * Create a fox. A fox can be created as a new born (age zero
+     * Create a bear. A bear can be created as a new born (age zero
      * and not hungry) or with a random age and food level.
      * 
-     * @param randomAge If true, the fox will have random age and hunger level.
+     * @param randomAge If true, the bear will have random age and hunger level.
      * @param field The field currently occupied.
      * @param location The location within the field.
      */
@@ -56,11 +56,11 @@ public class Bear extends Animal implements Actor
     }
     
     /**
-     * This is what the fox does most of the time: it hunts for
+     * This is what the bear does most of the time: it hunts for
      * rabbits. In the process, it might breed, die of hunger,
      * or die of old age.
      * @param field The field currently occupied.
-     * @param newFoxes A list to return newly born foxes.
+     * @param newBears A list to return newly born bears.
      */
     public void act(List<Actor> newActors)
     {
@@ -86,7 +86,7 @@ public class Bear extends Animal implements Actor
     }
 
     /**
-     * Increase the age. This could result in the fox's death.
+     * Increase the age. This could result in the bear's death.
      */
     private void incrementAge()
     {
@@ -97,7 +97,7 @@ public class Bear extends Animal implements Actor
     }
     
     /**
-     * Make this fox more hungry. This could result in the fox's death.
+     * Make this bear more hungry. This could result in the bear's death.
      */
     private void incrementHunger()
     {
@@ -141,13 +141,13 @@ public class Bear extends Animal implements Actor
     }
     
     /**
-     * Check whether or not this fox is to give birth at this step.
+     * Check whether or not this bear is to give birth at this step.
      * New births will be made into free adjacent locations.
-     * @param newFoxes A list to return newly born foxes.
+     * @param newBears A list to return newly born Bears.
      */
-    private void giveBirth(List<Actor> newFoxes)
+    private void giveBirth(List<Actor> newBears)
     {
-        // New foxes are born into adjacent locations.
+        // New bears are born into adjacent locations.
         // Get a list of adjacent free locations.
         Field field = getField();
         List<Location> free = field.getFreeAdjacentLocations(getLocation());
@@ -155,7 +155,7 @@ public class Bear extends Animal implements Actor
         for(int b = 0; b < births && free.size() > 0; b++) {
             Location loc = free.remove(0);
             Bear young = new Bear(false, field, loc);
-            newFoxes.add(young);
+            newBears.add(young);
         }
     }
         
