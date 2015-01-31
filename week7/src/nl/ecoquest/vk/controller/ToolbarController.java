94d93b3eff90f06de9fe172734936be25a73b733
atmino <angelanieboer@gmail.com>
2015-02-01 00:16:03 +0100

    rename class

---
+++ week7/src/nl/ecoquest/vk/controller/ToolbarController.java
@@ -0,0 +1,27 @@
+package nl.ecoquest.vk.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import nl.ecoquest.vk.model.*;
+import nl.ecoquest.vk.simulation.*;
+import nl.ecoquest.vk.view.*;
+
+public class ToolbarController extends AbstractController implements ActionListener {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ToolbarController(SimulatorModel model) {
+		super(model);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
