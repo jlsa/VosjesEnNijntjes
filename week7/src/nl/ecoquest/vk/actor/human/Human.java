87b9f5f3cb1aada447b986723c349a4c7c24fb39
jlsa <jlsahoekstra@gmail.com>
2015-02-01 20:03:35 +0100

    Actors human en hunter vergeten commentaar toe te voegen + hunter 'jaagt' nu ook op grass. Het wordt kapot gestapt. De andere dieren hebben nu de mogelijkheid om te ontsnappen aan de Hunter doormiddel van de methode tryToEscape mocht deze inhoud hebben

--- week7/src/nl/ecoquest/vk/actor/human/Human.java
+++ week7/src/nl/ecoquest/vk/actor/human/Human.java
@@ -6,16 +6,30 @@ import nl.ecoquest.vk.actor.*;
 import nl.ecoquest.vk.simulation.Field;
 import nl.ecoquest.vk.simulation.Location;
 
+/**
+ * A human is just to abstract. It definitely is an actor
+ * @author Jeroen, Angela en Joel
+ * @version 2015-02-01
+ */
 public abstract class Human implements Actor {
+	// The field on which the human exists
 	private Field field;
+	// The location on the field the human stands
 	private Location location;
+	// The active state of the human
 	private boolean active;
 	
+	/**
+	 * Constructor
+	 * @param field
+	 * @param location
+	 */
 	public Human(Field field, Location location) {
 		this.field = field;
 		this.location = location;
 		active = true;
 	}
+	
 	@Override
 	public boolean isActive() {
 		return active;
