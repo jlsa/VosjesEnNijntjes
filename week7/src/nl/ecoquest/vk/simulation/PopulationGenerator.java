cd420c7e675d5b5020b11ffe7b8c1edf3400529a
atmino <angelanieboer@gmail.com>
2015-02-02 15:04:45 +0100

    Foutje, bedankt
    FOR GREAT JUSTICE!

---
+++ week7/src/nl/ecoquest/vk/simulation/PopulationGenerator.java
@@ -0,0 +1,32 @@
+package nl.ecoquest.vk.simulation;
+
+/**
+ * The PopulationGenerator exists to generate the population
+ * This is a Singleton
+ * @author Angela
+ *
+ */
+
+public class PopulationGenerator {
+	
+	private static PopulationGenerator instance = null;
+	
+	/**
+	 * We only want one instance of PopulationGenerator
+	 */
+	private PopulationGenerator(){}
+	
+	public static PopulationGenerator getInstance(){
+		if(instance == null){instance = new PopulationGenerator();}
+		return instance;
+	}
+	
+	public static void generate(){
+		
+	}
+	
+	public static void setNewValues(){
+		
+	}
+	
+}
